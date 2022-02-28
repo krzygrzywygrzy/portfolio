cd7ebba4c8dcdad0805fd78985447202d37ecd67
trikitirem <trikitirem@outlook.com>
2022-02-28 21:39:36 +0100

    bandmate update

--- components/projects/TechDisplay.js
+++ components/projects/TechDisplay.js
@@ -3,7 +3,7 @@ import TechIcon from "../techIcon/TechIcon";
 
 const TechDisplay = ({ tech }) => {
   return (
-    <section className="w-full mt-4 pb-4 inline-flex justify-center md:jutify-start border-b border-stone-600">
+    <section className="w-full md:w-auto mt-4 pb-4 inline-flex justify-center md:justify-items-start border-b border-stone-600">
       {tech.map((t) => (
         <div key={t.desc} className="mr-4">
           <TechIcon img={t.img} desc={t.desc} link={t.link} />
