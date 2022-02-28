34bdad523379c8aa4167c2aca8ce157576130140
trikitirem <trikitirem@outlook.com>
2022-02-28 21:01:43 +0100

    update

--- pages/bandmate.js
+++ pages/bandmate.js
@@ -40,7 +40,7 @@ const bandmate = () => {
       <Head>
         <title>BandMate</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display:wght@500&display=swap"
           rel="stylesheet"
