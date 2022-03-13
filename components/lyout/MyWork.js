52735bd15665928162161a06793e3c29a40be57c
trikitirem <trikitirem@outlook.com>
2022-03-13 22:40:43 +0100

    MyWork section animation

--- components/lyout/MyWork.js
+++ components/lyout/MyWork.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import FeaturedProject from "../projects/FeaturedProject";
 import Project from "../projects/Project";
 import ThisPortoflio from "../projects/ThisPortoflio";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 const react_projects = [
   {
@@ -30,12 +32,44 @@ const react_projects = [
 ];
 
 const MyWork = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  const titleRef = useRef();
+  const projectsRef = useRef();
+
+  useEffect(() => {
+    gsap.to(titleRef.current, {
+      opacity: 1,
+      duration: 0.8,
+      y: -80,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: titleRef.current,
+      },
+    });
+
+    gsap.to(projectsRef.current, {
+      opacity: 1,
+      duration: 0.8,
+      y: -60,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: projectsRef.current,
+      },
+    });
+  }, []);
+
   return (
     <div id="mywork" className="py-32 md:py-24">
-      <header className="site text-center noto-serif text-6xl md:text-7xl mb-12">
+      <header
+        className="site text-center noto-serif text-6xl md:text-7xl mb-12 opacity-0"
+        ref={titleRef}
+      >
         My work
       </header>
-      <div className="site flex flex-col-reverse md:flex-row gap-4 justify-center">
+      <div
+        className="site flex flex-col-reverse md:flex-row gap-4 justify-center opacity-0"
+        ref={projectsRef}
+      >
         <div className="flex flex-col gap-8 md:w-96 ">
           {react_projects.map((project) => (
             <Project key={project.title} data={project} />
